f42af5dda83e585158f16e71bb5ad5d6356394a4
dmithree <koorany@gmail.com>
2019-12-10 19:18:14 +0300

    deleted Theo

--- bin/__fixtures__/setup.js
+++
@@ -1,9 +0,0 @@
-module.exports = theo => {
-  theo.registerFormat("array.js", result => {
-    let items = result
-      .get("props")
-      .map(prop => `\n  ['${prop.get("name")}', '${prop.get("value")}'],`)
-      .toJS();
-    return `module.exports = [${items}\n]`;
-  });
-};
